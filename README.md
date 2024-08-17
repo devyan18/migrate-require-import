149be3074df57146cb6ad9ba7e849b1cbb05f475
devyan18 <epache17@gmail.com>
2024-08-16 21:59:45 -0300

    add file upload manager

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # CRUD_task_backend
 
 ## FUNCIONALIDAD
+
 En este trabajo se podran Crear, Mostrar, Editar y Borrar las tareas que se encuentran en una base de datos sql
-Gracias a los codigos y utilizando el Thunder Cliente se podran realizar lo ya mencionado anteriormente. 
+Gracias a los codigos y utilizando el Thunder Cliente se podran realizar lo ya mencionado anteriormente.
--- src/middlewares/storage.middleware.js
+++ src/middlewares/storage.middleware.js
@@ -1,9 +1,10 @@
 import multer from "multer";
-import path from "path";
-import crypto from "crypto";
+import path from "node:path";
+import crypto from "node:crypto";
 
 const __dirname = path.resolve(path.dirname(""));
 
+// config of storage for multer
 export const storage = multer.diskStorage({
   destination: (_req, _file, cb) => {
     const destination = path.join(__dirname, "storage");
@@ -19,6 +20,7 @@ export const storage = multer.diskStorage({
   },
 });
 
+// middleware to upload single file
 export const uploadSingle = (fieldName) => {
   return multer({ storage }).single(fieldName);
 };
